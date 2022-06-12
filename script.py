40407673ee776e51985e16efb3e6375162bef53c
larspeterjeppesen <55874728+larspeterjeppesen@users.noreply.github.com>
2022-06-12 15:42:15 +0200

    Update script.py

--- script.py
+++ script.py
@@ -3,11 +3,15 @@ import subprocess as sp
 import pandas as pd
 from io import StringIO
 
-# cmd = 'ffprobe -show_entries stream=index:stream_tags=language,title test.mkv'
+#Get file format
+fileformat = input("Input video file format of your files: ")
+if (fileformat[0] != '.'):
+    fileformat = '.' + fileformat
 
-files = [f for f in os.listdir('.') if os.path.isfile(f) and f[-4:] == '.mkv']
+#Get list of media files to edit
+files = [f for f in os.listdir('.') if os.path.isfile(f) and f[-4:] == fileformat]
 
-#Audio
+#Audio selection
 cmd = 'ffprobe -loglevel error -select_streams a -show_entries stream=index:stream_tags=language,title -of csv=p=0 "{file}"'.format(file=files[0])
 audoutput = sp.getoutput(cmd)
 audoutput = pd.read_csv(StringIO(audoutput), names=['number', 'language', 'title'])
@@ -16,7 +20,7 @@ print("----- Audio Selection -----")
 print(audoutput.to_string(index=False))
 aid = input("Input number of desired audio: ")
 
-#Subtitles
+#Subtitle selection
 cmd = 'ffprobe -loglevel error -select_streams s -show_entries stream=index:stream_tags=language,title -of csv=p=0 "{file}"'.format(file=files[0])
 suboutput = sp.getoutput(cmd)
 suboutput = pd.read_csv(StringIO(suboutput), names=['number', 'language', 'title'])
@@ -25,17 +29,16 @@ print('----- Subtitle Selection -----')
 print(suboutput.to_string(index=False))
 sid = input("Input number of desired subtitle: ")
 
-
-cmd = 'ffmpeg -y -i \"{file}\" -map 0 -c copy -disposition:a -default-forced -disposition:a:{aid} default+forced -disposition:s -default-forced -disposition:s:{sid} default+forced \"{file}.out.mkv\"'
+#Modify forced/default audio/subtitle tracks
+cmd = 'ffmpeg -y -i \"{file}\" -map 0 -c copy -disposition:a -default-forced -disposition:a:{aid} default+forced -disposition:s -default-forced -disposition:s:{sid} default+forced \"{file}.out{fileformat}\"'
 
 print('Starting format...')
 for f in files:
-    if (f[-4:] == '.mkv'):
-        f_cmd = cmd.format(file=f, aid = aid, sid = sid)
-        output = sp.getoutput(f_cmd)
-        os.chmod(f, 0o777)
-        os.remove(f)
-        os.rename(f+'.out.mkv', f)
-        print('Converted', f)
+    f_cmd = cmd.format(file=f, aid = aid, sid = sid, fileformat = fileformat)
+    output = sp.getoutput(f_cmd)
+    os.chmod(f, 0o777)
+    os.remove(f)
+    os.rename(f+'.out{fileformat}'.format(fileformat=fileformat), f)
+    print('Converted', f)
 
 print('Finished converting all files')
